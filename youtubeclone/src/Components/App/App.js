9ebbf15889267145bc032319174ac6a51f4174b4
khizarSulehria <khizar.sulehri.ks@gmail.com>
2019-04-14 22:22:56 +0500

    youtube clone with react and youtube Apis

---
+++ youtubeclone/src/Components/App/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+
+//components
+
+class App extends React.Component{
+    render() {
+        return <div>App</div>;
+    }
+
+}
---
+++ youtubeclone/src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+
+//components
+import App from './Components/App';
+ReactDom.render(<App />,document.querySelector('#root'));
+
